3f1c9cc85585ce9cf8860244b07bce3adcd6cb77
Addy-codes <adeeb9839@gmail.com>
2023-05-12 21:42:36 +0530

    templates added

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ app.secret_key = "super secret key"
 
 @app.route("/")
 def index():
+    # if session['loggedin']==True :
+
     return render_template("index.html")
 
 
@@ -26,8 +28,8 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        print(username)
-        print(password)
+        # print(username)
+        # print(password)
         cursor.execute(
             "SELECT * FROM user WHERE username=%s AND password=%s",
             (username, password),
@@ -38,10 +40,10 @@ def login():
         if record:
             session["loggedin"] = True
             session["username"] = record[1]
-            print("Found")
-            return render_template("index.html", msg=msg)
+            # print("Found")
+            return render_template("startpage.html", msg=msg)
         else:
-            print("Not Found")
+            # print("Not Found")
             msg = "Incorrect username/password.Try again!"
             return render_template("login.html", msg=msg)
     return render_template("login.html")
@@ -50,6 +52,21 @@ def login():
 @app.route("/about", methods=["GET", "POST"])
 def about():
     return render_template("about.html")
+
+
+@app.route("/startpage", methods=["GET", "POST"])
+def startpage():
+    return render_template("startpage.html")
+
+
+@app.route("/about", methods=["GET", "POST"])
+def scorecard():
+    return render_template("scorecard.html")
+
+
+@app.route("/userdetail", methods=["GET", "POST"])
+def userdetail():
+    return render_template("userdetail.html")
 
 
 if __name__ == "__main__":
